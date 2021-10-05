31cecd41e9e62a1aba84ce967901260634f148c3
Mateusz Piwowarski <skyperpl@gmail.com>
2021-10-05 14:35:04 +0200

    #2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(3001, () =>{
-    console.log('Server initialization success')
-})
+const db = require('./models');
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log('Server initialization success');
+  });
+});
---
+++ server/models/users.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dateOfBirth: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    login: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isDeleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
+
+  return users;
+};
