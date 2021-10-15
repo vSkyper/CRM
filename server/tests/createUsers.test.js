accf8c9ea3c9ddad4864eabe96a1ee39349b94c8
Mateusz Piwowarski <skyperpl@gmail.com>
2021-10-15 21:32:10 +0200

    #12

--- server/tests/createUsers.test.js
+++ server/tests/createUsers.test.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 const faker = require('faker');
 
 test('createUsers', () => {
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 55; i++) {
     const data = {
       name: faker.name.firstName(),
       surname: faker.name.lastName(),
       dateOfBirth: faker.date.between('1960-01-01', '2006-01-01'),
-      login: 'hailey.sporer67',
+      login: faker.internet.userName(),
       password: faker.internet.password(),
     };
 
