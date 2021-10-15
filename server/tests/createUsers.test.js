80246f6b73f52c186268af587500ad162f8ea964
Mateusz Piwowarski <skyperpl@gmail.com>
2021-10-15 19:43:04 +0200

    #10

---
+++ server/tests/createUsers.test.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const faker = require('faker');
+
+test('createUsers', () => {
+  for (let i = 0; i < 10; i++) {
+    const data = {
+      name: faker.name.firstName(),
+      surname: faker.name.lastName(),
+      dateOfBirth: faker.date.between('1960-01-01', '2006-01-01'),
+      login: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+
+    axios.post('http://localhost:3001/createUser', data);
+  }
+});
